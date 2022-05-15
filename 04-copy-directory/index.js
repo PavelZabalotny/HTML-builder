262eeffcf6ff3c74298a3065320f32ac9893d3d0
PavelZabalotny <pavelzabalotny@gmail.com>
2022-05-15 22:24:50 +0300

    feat: implement 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,45 @@
+const path = require('path');
+const fs = require('fs');
 
+const pathDest = path.join(__dirname, 'files-copy');
+const pathSrc = path.join(__dirname, 'files');
+
+function readFolder(folder) {
+  fs.promises.readdir(pathDest, { withFileTypes: true })
+    .then((files) => {
+      files.forEach((file) => {
+        if (file.isFile()) {
+          fs.unlink(path.join(pathDest, file.name), (err) => {
+            if (err) {
+              throw err;
+            }
+            console.log(`${file.name} - was deleted!!!`);
+          });
+        }
+      });
+    })
+    .then(() => {
+      fs.promises.readdir(folder, { withFileTypes: true }).then((files) => {
+        files.forEach(file => {
+          if (file.isFile()) {
+            fs.copyFile(path.join(pathSrc, file.name), path.join(pathDest, file.name), (err) => {
+              if (err) {
+                throw err;
+              }
+            });
+            console.log(`${file.name} - was copied!!!`);
+          }
+        });
+      });
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
+
+fs.mkdir(pathDest, { recursive: true }, (err) => {
+  if (err) {
+    throw err;
+  }
+  readFolder(pathSrc);
+});
